68091deae62b6d4f0f713d57bf8a65269b0d68bd
JManion32 <jmanion32@gmail.com>
2026-05-07 01:07:20 -0400

    Removed Stylelint Rules that were causing Errors
    Will add these rules back when I get rid of Tailwind.

--- site/stylelint.config.js
+++ site/stylelint.config.js
@@ -1,8 +1,18 @@
 /** @type {import("stylelint").Config} */
 export default {
     extends: ['stylelint-config-standard'],
-    ignoreFiles: ['dist/**'],
+    ignoreFiles: ['dist/**', 'node_modules/**', 'public/**', 'src/assets/**'],
     rules: {
         'declaration-no-important': true,
+
+        'color-function-notation': null,
+        'alpha-value-notation': null,
+        'color-function-alias-notation': null,
+        'import-notation': null,
+        'media-feature-range-notation': null,
+        'keyframes-name-pattern': null,
+        'at-rule-no-unknown': null,
+        'no-duplicate-selectors': null,
+        'color-hex-length': null,
     },
 };
